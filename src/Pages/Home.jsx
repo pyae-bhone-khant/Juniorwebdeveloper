177dabfa0798c445dcbce239b01d25ac163e0c65
pyae-bhone-khant <pyaebhonen4@gmail.com>
2025-10-11 23:24:59 +0630

    I add LanguageCard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,17 +3,17 @@ import LanguageCard from '../components/LanguageCard';
 export default function Home() {
   return ( 
      <div> 
-    <div  className='flex  justify-between w-full h-120 mb-0  bg-blue-950' >
-   <div className='text-white flex flex-col px-20  mt-35 ' >
-    <h1 className='text-4xl font-bold' >Become a full stack Developer</h1>
-    <p className='mt-8 text-xl' >Learn web development step-by-step with our free YouTube course and complete guidebook. Perfect for beginners who want to start coding confidently.</p>
+    <div  className='flex flex-col lg:flex-row md:flex-row items-center justify-center  md:justify-between w-full h-full md:h-120 mb-0  bg-blue-950' >
+   <div className='text-white flex flex-col px-8 md:px-20 mt-15  md:mt-18 ' >
+    <h1 className=' text-2xl md:text-4xl font-bold' >Become a full stack Developer</h1>
+    <p className='mt-8  text-xl md:text-xl' >Learn web development step-by-step with our free YouTube course and complete guidebook. Perfect for beginners who want to start coding confidently.</p>
     <div className='mt-10 flex  gap-4' >
       <button className='border border-blue-900 px-2 py-2 hover:opacity-80 rounded bg-white text-blue-900 font-bold  text-xl'>🎥 Watch Course</button>
-      <button className='border border-white px-2 hover:opacity-80 py-2 rounded text-white font-bold text-xl'>📘 Read the Book</button>
+      <button className='border border-white px-2 hover:opacity-80 py-2  rounded text-white font-bold text-xl'>📘 Read the Book</button>
     </div>
    </div> 
     <div>
-      <img src={header} className='w-300 h-full' alt="" />
+      <img src={header} className= ' mt-7 md:mt-0 md:w-250 lg:w-300 lg:h-120 h-full' alt="" />
     </div>
     </div> 
     <LanguageCard />
--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -1,10 +1,60 @@
+const Language = [
+    {
+        id: 1,
+        img: "javascript.svg",
+        title: "HTML",
+    },
+    {
+        id: 2,
+        img: "javascript.svg", 
+        title: "CSS",
+    },
+     {
+        id: 3,
+        img: "javascript.svg", 
+        title: "Javascript",
+    },
+     {
+        id: 4,
+        img: "javascript.svg", 
+        title: "React",
+    },
+     {
+        id: 5,
+        img: "javascript.svg", 
+        title: "Next.js",
+    },
+     {
+        id: 6,
+        img: "javascript.svg", 
+        title: "Node.js",
+    },
+    
+]
+
+
 export default function LanguageCard() {
     return (
-       <div className="w-full h-300 mt-0 bg-gray-200" >
-        <div className=" flex justify-center gap-2  py-10" >
-           <h1 className="text-black text-4xl font-bold hover:underline">We collaborate with</h1>
-           <h2 className="text-blue-900 text-4xl font-bold hover:underline"> 20 youtube channel </h2>
-        </div>
+       <div className="w-full h-full mt-0 bg-gray-300" >
+        <div className=" flex justify-center gap-2 pt-6  md:pt-20" >
+           <h1 className="text-black text-xm  md:text-2xl lg:text-4xl font-bold hover:underline">We collaborate with</h1>
+           <h2 className="text-blue-900 text-xm md:text-2xl lg:text-4xl font-bold hover:underline"> 20 Youtube channel and 10 Books </h2>
+        </div>   
+                     <div className="flex justify-center pt-6 md:pt-10  ">
+               <h1 className="text-blue-800 text-xm  md:text-2xl lg:text-4xl font-bold ">Learn everything you need to know</h1>
+                     </div> 
+                     <div className="flex justify-center items-center p-10 px-10">
+
+                   <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mt-10 " >
+                      {Language.map(lang => (
+                          <div key={lang.id} className="p-4 mb-4 ml-4 bg-gray-100 rounded-xl w-60 cursor-pointer hover:scale-105
+               transition-all duration-300 ease-in-out h-80 text-center shadow">
+                                <img src={lang.img} alt={lang.title} className=" mt-5 w-40 h-40 mx-auto mb-4" />
+                             <h2 className="text-2xl mt-10 text-blue-900 font-semibold">{lang.title}</h2>
+                                     </div>
+                                           ))}
+  </div>
+                     </div>
        </div>
     )
 }
